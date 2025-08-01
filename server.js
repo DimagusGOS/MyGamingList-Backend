67bbf1850ebacb452091b7b1aeade1b8183c6abb
Diego <diegomano2@hotmail.com>
2025-07-31 18:17:18 -0700

    fixing deployment to work properly

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose
 const app = express();
 app.use(cors({
     // origin: 'http://localhost:5173', // local origin
-    origin: 'https://my-gaming-list-tau.vercel.app/', // render origin
+    origin: 'https://my-gaming-list-tau.vercel.app', // render origin
     credentials: true
 }));
 app.use(express.json());
